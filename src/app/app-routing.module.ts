0d70fae4f8ef6f3394af65d139b5715a76644e02
grujicdragan <dragan_grujic93@hotmail.com>
2020-04-06 16:54:29 +0200

    fixed router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './components/category-component/category/category.component';
 import { MealComponent } from './components/meal-component/meal/meal.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
-import { ContactComponent } from './components/contact/contact.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponentComponent },
   { path: 'meals', component: MealComponent },
   { path: 'category', component: CategoryComponent },
-  { path: ' ', redirectTo: '/home'},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
 
 @NgModule({
