c8649bbd9ca6b53fed4c37430533d9c704d20ddb
grujicdragan <dragan_grujic93@hotmail.com>
2020-04-17 14:26:32 +0200

    adadada

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const MaterialComponents = [
   MatButtonModule,
